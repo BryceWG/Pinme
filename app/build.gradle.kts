44b0e8212dd89f9c78d8b4b0ccbcc28457f081c0
BryceWG <zhang1577006721@qq.com>
2025-12-15 23:50:16 +0800

    build(version): 重置版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.brycewg.pinme"
         minSdk = 33
         targetSdk = 36
-        versionCode = 30
-        versionName = "1.0.30"
+        versionCode = 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
