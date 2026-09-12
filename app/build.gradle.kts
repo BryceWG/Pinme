d92dc822fd15fd3170a435522a5268555ad0de47
BryceWG <zhang1577006721@qq.com>
2026-09-12 15:22:47 +0800

    build(app): 提升版本至 1.4.1 (versionCode 11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.brycewg.pinme"
         minSdk = 33
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.4.0"
+        versionCode = 11
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
